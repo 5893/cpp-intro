f77d0d1156c9895e3970c86267e74462b9544370
5893 <goto5893@gmail.com>
2022-01-09 22:35:20 +0900

    変数宣言その他の方法

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "all.h"
 
 int main() {
+    // 変数の最初の値は = 値 の代わりに {値}や(値)と書いても良い
+    auto a = 1;
+    auto b(2);
+    auto c{3};
+
+    // 具体的な型を書いた初期化。autoで宣言しても後から代入で別の型は入れられない。
+    int i = 123;
+    double d = 1.23;
+    std::string s = "123"s;
+
     // 整数の変数
     auto answer = 42;
     std::cout << answer << "\n"s;
