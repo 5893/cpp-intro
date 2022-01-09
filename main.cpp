9ba0e8655994f37255c6348f3490e54d2969bc0f
5893 <goto5893@gmail.com>
2022-01-09 21:58:32 +0900

    文字列の標準出力

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "all.h"
 
 int main() {
-    std::cout << "Hello, World!"s << std::endl;
+    std::cout << "Hello, World!\n"
+              << "改行可能\n"
+                 // operator << しなくても出力的には連結される
+                 "\\を使う場合は更に" + "エスケープが必要"s // operator + で連結も可能
+              << std::endl;
     return 0;
 }
