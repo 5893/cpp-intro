bc31717d4dcd24aac19b5981e2286548411325e6
5893 <goto5893@gmail.com>
2022-01-10 11:22:07 +0900

    for

--- main.cpp
+++ main.cpp
@@ -1,24 +1,10 @@
 #include "all.h"
 
 int main() {
-
-    // 短絡評価
-    // 左から順に最小に評価するので、この場合はbは評価されない ← 実行すらされない
-
-    // ラムダ式は [](引数){ 処理 } で定義可能
-    auto a = []()
+    for (auto i = 0; i < 100; i++)
     {
-        std::cout << "a\n"s;
-        return false;
-    };
-    auto b = []()
-    {
-        std::cout << "b\n"s;
-        return true;
-    };
-
-    bool c = a() && b();
-    std::cout << std::boolalpha << c;
+        std::cout << "count = "s << i << "\n";
+    }
 
     return 0;
 }
