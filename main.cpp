d7f3caa92ee59c115e2653abfbfe02a453893915
5893 <goto5893@gmail.com>
2022-01-10 21:36:58 +0900

    array実装

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include "all.h"
 
-// 複数設定も可能
-// T N : T型の値N
-template < typename T, T N >
-T value()
+template < typename T, std::size_t N >
+struct array
 {
-    return N;
-}
+    using value_type = T;
+    using reference = T &;
+
+    using size_type = std::size_t;
+
+    value_type storage[N];
+
+    reference operator [] ( size_type i )
+    {
+        return storage[i];
+    }
+};
 
 int main() {
-    value<int, 1>();
-    value<short, 1>();
+    using array = std::array<int, 5>;
+    array a = {1,2,3,4,5};
+
+    array::value_type x = 0;
+    array::reference ref = a[0];
+
+    auto print = [](auto first, auto last) {
+        for (auto iter = first; iter != last; ++iter)
+        {
+            std::cout << *iter << "\n"s;
+        }
+    };
+    print( a.begin(), a.end() );
 }
