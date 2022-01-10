e593b371602b0a4a224ad50d53739db9c704eb40
5893 <goto5893@gmail.com>
2022-01-10 12:16:09 +0900

    変数名に使ってはならない名前

--- main.cpp
+++ main.cpp
@@ -1,26 +1,21 @@
 #include "all.h"
 
 int main() {
-    // int型の最小値と最大値
-    std::cout
-        << std::numeric_limits<int>::min() << "\n"s
-        << std::numeric_limits<int>::max() << "\n"s;
+    // 使ってはならない
 
-    // unsigned int型の最小値と最大値
-    std::cout
-        << std::numeric_limits<unsigned int>::min() << "\n"s
-        << std::numeric_limits<unsigned int>::max() << "\n"s;
+    // ダブルアンダースコアを含む名前
+    // C++コンパイラがその名前をC++実装の為に予約されている
+    int __ = 0;
+    int a__ = 0;
+    int __a = 0;
 
-    // 最小値に-1したり、最大値に+1したりすると、
-    // 最小値は最大値に、最大値は最小値に反転する → ビットで考えると自然
-    int min = std::numeric_limits<int>::min() ;
-    int max = std::numeric_limits<int>::max() ;
+    // アンダースコアに大文字から始まる
+    // C++コンパイラがその名前をC++実装の為に予約されている
+    int _A = 0;
 
-    int min_minus_one = min - 1 ;
-    int max_plus_one = max + 1 ;
-
-    std::cout << "min="s << min << "\n"s << "max="s << max << "\n"s
-              << "min-1="s << min_minus_one << "\n"s << "max+1=" << max_plus_one << "\n"s;
+    // アンダースコアに小文字から始まる
+    // グローバル名前空間で予約されている
+    int _a = 0;
 
     return 0;
 }
