e9925b1332682e780f397c74ad7e4c3744971710
5893 <goto5893@gmail.com>
2022-01-10 14:59:04 +0900

    iterator

--- main.cpp
+++ main.cpp
@@ -1,18 +1,60 @@
 #include "all.h"
 
 int main() {
-    // エイリアス宣言
-    using Number = int;
+    {
+        std::vector<int> v = {1,2,3,4,5};
+        auto i = std::begin(v); // イテレータの取得
 
-    // Numberはintの別名
-    Number x = 0;
+        // イテレータの参照する要素へのアクセスは * 演算しを使う
+        int x = *i; // 1
 
-    // 昔はこういう型宣言の書き方を使っている。
-    // ライブラリを読むときには頻出なので覚えておくこと
-    typedef int Number;
+        *i = 0; // v = {0,2,3,4,5} となる
 
-    // この書き方はコンパイル通ってしまうが、型によっては通らないものがあるらしい
-    int typedef Number;
+        // イテレータの参照する要素を変更する
+
+        *i; // 0
+        ++i;
+        *i; // 2
+        ++i;
+        *i; // 3
+
+        --i;
+        *i; // 2
+        --i;
+        *i; // 0
+    }
+
+    {
+        // vectorの全要素を先頭からイテレータでアクセスするには、要素数分だけ++iすれば良いということ
+        std::vector<int> v = {1, 2, 3, 4, 5};
+        auto iter = std::begin(v);
+        for (std::size_t i = 0; i < std::size(v); ++i, ++iter)
+        {
+            std::cout << *iter << "\n"s;
+        }
+    }
+
+    {
+        // beginではなくendでイテレータを取得する
+        // end 最後の要素の”次”を示す
+        std::vector<int> v = {1,2,3,4,5};
+        auto i = std::end(v);
+
+        // *i; // エラー
+        --i;    // 最後の要素を示す
+        std::cout << "auto i = end(v) → --i の結果 *i = "s
+            << *i << "\n"s;   // 5
+    }
+
+    {
+        // endを使って、要素全てへアクセスするループをもう少し意味的に書き直すことができる
+        std::vector<int> v = {1,2,3,4,5};
+        for (auto iter = std::begin(v), last = std::end(v);
+            iter != last; ++iter)
+        {
+            std::cout << *iter << "\n"s;
+        }
+    }
 
     return 0;
 }
