cf75cfd7efc49d04ecd3b3da2ca6023e14f85c1d
5893 <goto5893@gmail.com>
2022-01-10 15:16:55 +0900

    iterator使った色々

--- main.cpp
+++ main.cpp
@@ -1,60 +1,36 @@
 #include "all.h"
 
+//auto output_all = []( auto first, auto last )
+//{
+//    for (auto iter = first; iter != last; ++iter)
+//    {
+//        std::cout << *iter << "\n"s;
+//    }
+//};
+
+auto output_all = [](auto first, auto last, auto output_iter)
+{
+    for (auto iter = first; iter != last; ++iter, ++output_iter)
+    {
+        *output_iter = *iter;
+    }
+};
+
 int main() {
-    {
-        std::vector<int> v = {1,2,3,4,5};
-        auto i = std::begin(v); // イテレータの取得
+    // 通常
+//    std::vector<int> v = {1,2,3,4,5};
+    // 標準入力から受け取るイテレータ
+    // 受け取った瞬間 output_all で出力される
+//    std::istream_iterator<int> first( std::cin ), last;
+    // カレントディレクトリファイル一覧を取得するイテレータ
+//    std::filesystem::directory_iterator first("./"), last;    // 何故か動かない……
+    // コピーさせることもできる
+    std::vector<int> source = {1,2,3,4,5};
+    std::vector<int> destination(5);
 
-        // イテレータの参照する要素へのアクセスは * 演算しを使う
-        int x = *i; // 1
-
-        *i = 0; // v = {0,2,3,4,5} となる
-
-        // イテレータの参照する要素を変更する
-
-        *i; // 0
-        ++i;
-        *i; // 2
-        ++i;
-        *i; // 3
-
-        --i;
-        *i; // 2
-        --i;
-        *i; // 0
-    }
-
-    {
-        // vectorの全要素を先頭からイテレータでアクセスするには、要素数分だけ++iすれば良いということ
-        std::vector<int> v = {1, 2, 3, 4, 5};
-        auto iter = std::begin(v);
-        for (std::size_t i = 0; i < std::size(v); ++i, ++iter)
-        {
-            std::cout << *iter << "\n"s;
-        }
-    }
-
-    {
-        // beginではなくendでイテレータを取得する
-        // end 最後の要素の”次”を示す
-        std::vector<int> v = {1,2,3,4,5};
-        auto i = std::end(v);
-
-        // *i; // エラー
-        --i;    // 最後の要素を示す
-        std::cout << "auto i = end(v) → --i の結果 *i = "s
-            << *i << "\n"s;   // 5
-    }
-
-    {
-        // endを使って、要素全てへアクセスするループをもう少し意味的に書き直すことができる
-        std::vector<int> v = {1,2,3,4,5};
-        for (auto iter = std::begin(v), last = std::end(v);
-            iter != last; ++iter)
-        {
-            std::cout << *iter << "\n"s;
-        }
-    }
+//    output_all(first, last);
+//    output_all(first, last, std::ostream_iterator<int>(std::cout));
+    output_all( std::begin(source), std::end(source), std::begin(destination) );
 
     return 0;
 }
