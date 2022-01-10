b7d037dfe1b7a72f41e127dd1005c0a3f148b58f
5893 <goto5893@gmail.com>
2022-01-10 15:59:09 +0900

    構造体とコンストラクトとデストラクタ

--- main.cpp
+++ main.cpp
@@ -9,11 +9,28 @@ struct fractional
     {
         return static_cast<double>(num) / static_cast<double>(denom);
     }
+
+    fractional(int num, int denom)
+    {
+        // コンストラクタ処理
+        fractional::num = num;
+        fractional::denom = denom;
+    }
+
+    ~fractional()
+    {
+        // デストラクタ処理
+    }
 };
 
 int main() {
     fractional x { 1, 2 };
     std::cout << x.value() << "\n"s;
+
+    {
+        // コンストラクタで初期化
+        auto a = fractional(5, 5);
+    }   // スコープを抜けて破棄される→デストラクタ発火
 
     return 0;
 }
