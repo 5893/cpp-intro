7691a6528546ca17d3c057971073fd8f9cb1246a
5893 <goto5893@gmail.com>
2022-01-10 15:24:52 +0900

    const

--- main.cpp
+++ main.cpp
@@ -1,46 +1,11 @@
 #include "all.h"
 
-void assign_3( int x )
-{
-    x = 3;  // コピーされた x は 元の a とは違うアドレスを指しているので、代入しても a には反映されない
-}
-
-void assign_3_ref (int & x )
-{
-    x = 3;
-}
-
 int main() {
-    {
-        int a = 1;
-        int b = 2;
+    int x = 0;
+    x = 1;
 
-        b = a;
-        // b == 1 aの値がコピーされて代入されている
-
-        b = 3;
-        // a == 1
-        // b == 3
-
-        assign_3( a );  // 値はコピーされて渡されている
-
-        // a == 1
-    }
-
-    {
-        int a = 1;
-        int & ref = a;  // 参照 ref は a を参照する
-
-        ref = 3;
-
-        // a == 3
-        // ref は a なので同じく 3
-
-        ref = 1;    // 一旦戻して
-        assign_3_ref(a);  // 関数は参照として受け取っているので、関数側でaへアクセス可能
-
-        // a == 3
-    }
+//    const int y = 0;
+//    y = 0;  // エラー、constは変更できない
 
     return 0;
 }
