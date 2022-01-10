4f2915bac39743e8e94750a5da5cac546ffc6f03
5893 <goto5893@gmail.com>
2022-01-10 11:28:19 +0900

    std::vector<T>型

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include "all.h"
 
 int main() {
-    for (auto i = 0; i < 100; i++)
+    // 整数型intの値を保持するvector
+    std::vector<int> vi;
+
+    // 浮動小数点型doubleの値を保持するvector
+    std::vector<double> vd;
+
+    // 文字列型std::stringの値を保持するvector
+    std::vector<std::string> vs;
+
+    std::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    for (int i = 4; i <= 1000; i++)
     {
-        std::cout << "count = "s << i << "\n";
+        v.push_back(i);
+    }
+
+    std::cout << "v's length: "s << v.size() << "\n"s;
+
+    // vの中身全部出力する
+    for (int i : v)
+    {
+        std::cout << i << "\n"s;
     }
 
     return 0;
