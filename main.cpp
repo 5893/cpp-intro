3215891d56444a454cab324a0848c2130acac64b
5893 <goto5893@gmail.com>
2022-01-10 17:57:40 +0900

    sizeofは型やオブジェクトのバイト数を取得する・size()は要素数を取得する

--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 
 int main() {
     auto print = [](auto s){ std::cout << s << "\n"s ; } ;
-    int a[5] ;
-    print( sizeof(a) ) ;        // 20: 要素型 * 要素数になる為、 5 * 4 = 20 となる
-    print( sizeof(int) );       // 4
-    print( sizeof(a) / sizeof(int) );   // こうやって要素数を出力すると良い
+    std::array<int, 5> a{};
 
-    double b [5] ;
-    print( sizeof(b) ) ;            // 40: 要素型 * 要素数になる為、 5 * 8 = 40 となる
-    print( sizeof(double) ) ;       // 8
-    print( sizeof(b) / sizeof(double) );   // こうやって要素数を出力すると良い
+    // aのバイト数
+    print( sizeof(a) );
+    // 要素数: 5
+    print( a.size() );
 }
