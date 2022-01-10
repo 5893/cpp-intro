8985514e710006fb33c6cc7552641a98a29b4135
5893 <goto5893@gmail.com>
2022-01-10 10:25:19 +0900

    bool値と出力

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
 #include "all.h"
 
 int main() {
-    // 変数の最初の値は = 値 の代わりに {値}や(値)と書いても良い
-    auto a = 1;
-    auto b(2);
-    auto c{3};
 
-    // 具体的な型を書いた初期化。autoで宣言しても後から代入で別の型は入れられない。
-    int i = 123;
-    double d = 1.23;
-    std::string s = "123"s;
+    auto a = 1 == 1;    // bool型
+    bool b = 2 == 2;
 
-    // 整数の変数
-    auto answer = 42;
-    std::cout << answer << "\n"s;
-    // 浮動小数点の変数
-    auto pi = 3.14;
-    std::cout << pi << "\n"s;
+    if (a) { std::cout << a << "\n"s; }     // 1 が出力される
+    if (b) { std::cout << b << "\n"s; }     // 1 が出力される
+    if (0) { std::cout << "boolでは0はfalse扱い"s << "\n"; } // 出力されない
+    std::cout << false << "\n"s;    // 0 が出力される
 
-    // 文字列の変数
-    auto question = "Lif, The Universe, and Everything."s;
-    std::cout << question;
+    // bool型の値を正しく出力するには std::boolalpha を出力する
+    // 出力後のbool値はtrue/falseで出力される
+    std::cout << std::boolalpha;
+    std::cout << true << "\n"s << false << "\n"s;
+
+    // 元に戻すには、std::noboolalphaを出力する
+    std::cout << std::noboolalpha;
+    std::cout << true << "\n"s << false << "\n"s;
 
     return 0;
 }
